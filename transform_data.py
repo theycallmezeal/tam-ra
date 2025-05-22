417f8073a0401d3a563d7442008ef7f75aa939f8
unknown <zealan@gmail.com>
2025-05-22 13:29:29 -0500

    first commit

---
+++ transform_data.py
@@ -0,0 +1,153 @@
+RESPONDENT_ID = 0
+AGE = 1
+GENDER = 2
+REGION = 5
+WORK = 6
+EDUCATION = 7
+LANGUAGES = 8
+LANG_EDUCATION = 9
+IKIGOYI = 12
+IKINYAGISAKA = 13
+IKINYAMBO = 14
+IKIRERA = 15
+IGIKIGA = 16
+MULTILINGUALISM = 17
+LG_MIXING = 18
+EVALUATE_LG_MIXING = 19
+
+conditions = [
+	[27, 'FUT0INDDP1', 'FUT', '0', 'INDDP'],
+	[30, 'FUT0INDDP2', 'FUT', '0', 'INDDP'],
+	[33, 'FUT0INDfinal', 'FUT', '0', 'INDfinal'],
+	[36, 'FUT0INDko1', 'FUT', '0', 'INDko'],
+	[39, 'FUT0INDko2', 'FUT', '0', 'INDko'],
+	[42, 'FUT0INDngo1', 'FUT', '0', 'INDngo'],
+	[45, 'FUT0INDngo2', 'FUT', '0', 'INDngo'],
+	[48, 'FUT0NEG1', 'FUT', '0', 'NEG'],
+	[51, 'FUT0NEG2', 'FUT', '0', 'NEG'],
+	[54, 'FUT0PART1', 'FUT', '0', 'PART'],
+	[57, 'FUT0PART2', 'FUT', '0', 'PART'],
+	[60, 'FUT0REL1', 'FUT', '0', 'REL'],
+	[63, 'FUT0REL2', 'FUT', '0', 'REL'],
+	[66, 'FUTraINDDP1', 'FUT', 'ra', 'INDDP'],
+	[69, 'FUTraINDDP2', 'FUT', 'ra', 'INDDP'],
+	[72, 'FUTraINDfinal1', 'FUT', 'ra', 'INDfinal'],
+	[75, 'FUTraINDfinal2', 'FUT', 'ra', 'INDfinal'],
+	[78, 'FUTraINDko1', 'FUT', 'ra', 'INDko'],
+	[81, 'FUTraINDko2', 'FUT', 'ra', 'INDko'],
+	[84, 'FUTraINDngo1', 'FUT', 'ra', 'INDngo'],
+	[87, 'FUTraINDngo2', 'FUT', 'ra', 'INDngo'],
+	[90, 'FUTraNEG1', 'FUT', 'ra', 'NEG'],
+	[93, 'FUTraNEG2', 'FUT', 'ra', 'NEG'],
+	[96, 'FUTraPART1', 'FUT', 'ra', 'PART'],
+	[99, 'FUTraPART2', 'FUT', 'ra', 'PART'],
+	[102, 'FUTraREL1', 'FUT', 'ra', 'REL'],
+	[105, 'FUTraREL2', 'FUT', 'ra', 'REL'],
+	[108, 'HAB0INDDP', 'HAB', '0', 'INDDP'],
+	[111, 'HAB0INDfinal', 'HAB', '0', 'INDfinal'],
+	[114, 'HAB0INDko', 'HAB', '0', 'INDko'],
+	[117, 'HAB0INDngo1', 'HAB', '0', 'INDngo'],
+	[120, 'HAB0INDngo2', 'HAB', '0', 'INDngo'],
+	[123, 'HAB0NEG', 'HAB', '0', 'NEG'],
+	[126, 'HAB0PART', 'HAB', '0', 'PART'],
+	[129, 'HAB0REL', 'HAB', '0', 'REL'],
+	[132, 'HABraINDDP', 'HAB', 'ra', 'INDDP'],
+	[135, 'HABraINDfinal', 'HAB', 'ra', 'INDfinal'],
+	[138, 'HABraINDko', 'HAB', 'ra', 'INDko'],
+	[141, 'HABraINDngo1', 'HAB', 'ra', 'INDngo'],
+	[144, 'HABraINDngo2', 'HAB', 'ra', 'INDngo'],
+	[147, 'HABraNEG', 'HAB', 'ra', 'NEG'],
+	[150, 'HABraPART', 'HAB', 'ra', 'PART'],
+	[153, 'HABraREL', 'HAB', 'ra', 'REL'],
+	[156, 'PROG0INDDP1', 'PROG', '0', 'INDDP'],
+	[159, 'PROG0INDDP2', 'PROG', '0', 'INDDP'],
+	[162, 'PROG0INDfinal', 'PROG', '0', 'INDfinal'],
+	[165, 'PROG0INDko1', 'PROG', '0', 'INDko'],
+	[168, 'PROG0INDko2', 'PROG', '0', 'INDko'],
+	[171, 'PROG0INDngo1', 'PROG', '0', 'INDngo'],
+	[174, 'PROG0INDngo2', 'PROG', '0', 'INDngo'],
+	[177, 'PROG0NEG1', 'PROG', '0', 'NEG'],
+	[180, 'PROG0NEG2', 'PROG', '0', 'NEG'],
+	[183, 'PROG0PART1', 'PROG', '0', 'PART'],
+	[186, 'PROG0PART2', 'PROG', '0', 'PART'],
+	[189, 'PROG0REL1', 'PROG', '0', 'REL'],
+	[192, 'PROG0REL2', 'PROG', '0', 'REL'],
+	[195, 'PROGraINDDP1', 'PROG', 'ra', 'INDDP'],
+	[198, 'PROGraINDDP2', 'PROG', 'ra', 'INDDP'],
+	[201, 'PROGraINDfinal1', 'PROG', 'ra', 'INDfinal'],
+	[204, 'PROGraINDfinal2', 'PROG', 'ra', 'INDfinal'],
+	[207, 'PROGraINDko1', 'PROG', 'ra', 'INDko'],
+	[210, 'PROGraINDko2', 'PROG', 'ra', 'INDko'],
+	[213, 'PROGraINDngo1', 'PROG', 'ra', 'INDngo'],
+	[216, 'PROGraINDngo2', 'PROG', 'ra', 'INDngo'],
+	[219, 'PROGraNEG1', 'PROG', 'ra', 'NEG'],
+	[222, 'PROGraNEG2', 'PROG', 'ra', 'NEG'],
+	[225, 'PROGraPART1', 'PROG', 'ra', 'PART'],
+	[228, 'PROGraPART2', 'PROG', 'ra', 'PART'],
+	[231, 'PROGraREL1', 'PROG', 'ra', 'REL'],
+	[234, 'PROGraREL2', 'PROG', 'ra', 'REL'],
+	[237, 'PROGpINDfinal', 'PROG', 'p', 'INDfinal'],
+	[240, 'PROGpINDDP', 'PROG', 'p', 'INDDP'],
+	[243, 'PROGpINDko', 'PROG', 'p', 'INDko'],
+	[246, 'PROGpINDngo', 'PROG', 'p', 'INDngo'],
+	[249, 'PROGpNEG', 'PROG', 'p', 'NEG'],
+	[252, 'PROGpPART', 'PROG', 'p', 'PART'],
+	[255, 'PROGpREL', 'PROG', 'p', 'REL'],
+]
+NUMBER = 0
+CONDITION_NAME = 1
+TAM = 2
+MORPHEME = 3
+FRAME = 4
+
+def reverse(likert):
+	if likert == 1:
+		return 5
+	if likert == 2:
+		return 4
+	if likert == 3:
+		return 3
+	if likert == 4:
+		return 2
+	return 1
+
+import csv
+with open('annotated_data.csv', mode='r', encoding='utf-8') as file:
+	with open('transformed_data.csv', mode='w', encoding='utf-8', newline='') as output:
+		# file.readline()
+		# ids = file.readline().split(',')
+		# for i in range(27, 257, 3):
+		# 	condition_name = ids[i].split('-')[0]
+		# 	tam = 'PROG' if 'PROG' in condition_name else 'HAB' if 'HAB' in condition_name else 'FUT'
+		# 	morpheme = 'ra' if 'ra' in condition_name else '0' if '0' in condition_name else 'p'
+		# 	frame = 'INDDP' if 'INDDP' in condition_name else 'INDfinal' if 'INDfinal' in condition_name else 'INDngo' if 'INDngo' in condition_name else 'INDko' if 'INDko' in condition_name else 'NEG' if 'NEG' in condition_name else 'PART' if 'PART' in condition_name else 'REL'
+		# 	print(str([i, condition_name, tam, morpheme, frame]) + ',')
+
+		csv_writer = csv.writer(output)
+		csv_writer.writerow(['CONDITION_NAME', 'TAM', 'MORPHEME', 'FRAME', 'WOULD_YOU_SAY_THIS', 'HAVE_YOU_HEARD_THIS', 'RESPONDENT_ID', 'AGE', 'GENDER', 'REGION',
+					  'IKIGOYI', 'IKINYAGISAKA', 'IKINYAMBO', 'IKIRERA', 'IGIKIGA', 'MULTILINGUALISM', 'LG_MIXING', 'EVALUATE_LG_MIXING'])
+		reader = csv.reader(file)
+		for index, line in enumerate(reader):
+
+			if index != 0 and index != 1:
+				for condition in conditions:
+					csv_writer.writerow([
+						condition[CONDITION_NAME],
+						condition[TAM],
+						condition[MORPHEME],
+						condition[FRAME],
+						reverse(line[condition[NUMBER]]),
+						line[condition[NUMBER] + 1],
+						line[RESPONDENT_ID],
+						line[AGE],
+						'female' if line[GENDER] == '1' else 'male',
+						line[REGION],
+						line[IKIGOYI],
+						line[IKINYAGISAKA],
+						line[IKINYAMBO],
+						line[IKIRERA],
+						line[IGIKIGA],
+						line[MULTILINGUALISM],
+						line[LG_MIXING],
+						line[EVALUATE_LG_MIXING]
+					])
